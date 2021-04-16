de56070be94f002e26600c7b4f21037547338d94
John Codeos <johncodeos@gmail.com>
2021-04-16 16:26:24 +0300

    Fix app.js syntax

--- app.js
+++ app.js
@@ -8,53 +8,55 @@ const jwt = require('jsonwebtoken');
 
 const getClientSecret = () => {
 
-        const time = new Date().getTime() / 1000; // Current time in seconds since Epoch
-        const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_FILE);
+  const time = new Date().getTime() / 1000; // Current time in seconds since Epoch
+  const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_FILE);
 
-        const headers = {
-                kid: process.env.KEY_ID,
-                typ: undefined
-        }
+  const headers = {
+    kid: process.env.KEY_ID,
+    typ: undefined
+  }
 
-        const claims = {
-                'iss': process.env.TEAM_ID,
-                'iat': time, // The time the token was generated
-                'exp': time + 86400 * 180, // Token expiration date
-                'aud': 'https://appleid.apple.com',
-                'sub': process.env.SERVICE_ID,
-        }
+  const claims = {
+    'iss': process.env.TEAM_ID,
+    'iat': time, // The time the token was generated
+    'exp': time + 86400 * 180, // Token expiration date
+    'aud': 'https://appleid.apple.com',
+    'sub': process.env.SERVICE_ID,
+  }
 
-        token = jwt.sign(claims, privateKey, {
-                algorithm: 'ES256',
-                header: headers
-        });
+  token = jwt.sign(claims, privateKey, {
+    algorithm: 'ES256',
+    header: headers
+  });
 
-        return token
+  return token
 }
 
-app.post('/callback', bodyParser.urlencoded({ extended: true }), (req, res) => {
-        if (res.statusCode == 200) {
-                var returnURL = ""
-                var firstName = ""
-                var middleName = ""
-                var lastName = ""
-                var email = ""
-                if (req.body.hasOwnProperty('user')) {
-                        const userdata = req.body.user
-                        const user = JSON.parse(userdata)
-                        firstName = '&first_name=' + user.name['firstName']
-                        middleName = '&middle_name=' + user.name['middleName']
-                        lastName = '&last_name=' + user.name['lastName']
-                        email = '&email=' + user.email
-                }
+app.post('/callback', bodyParser.urlencoded({
+  extended: true
+}), (req, res) => {
+  if (res.statusCode == 200) {
+    var returnURL = ""
+    var firstName = ""
+    var middleName = ""
+    var lastName = ""
+    var email = ""
+    if (req.body.hasOwnProperty('user')) {
+      const userdata = req.body.user
+      const user = JSON.parse(userdata)
+      firstName = '&first_name=' + user.name['firstName']
+      middleName = '&middle_name=' + user.name['middleName']
+      lastName = '&last_name=' + user.name['lastName']
+      email = '&email=' + user.email
+    }
 
-                var code = '&code=' + req.body.code
-                var clientSecret = '&client_secret=' + getClientSecret()
-                returnURL = '?success=true' + code + clientSecret + firstName + middleName + lastName + email
-                res.redirect(returnURL)
-        } else {
-                res.redirect('?success=false')
-        }
+    var code = '&code=' + req.body.code
+    var clientSecret = '&client_secret=' + getClientSecret()
+    returnURL = '?success=true' + code + clientSecret + firstName + middleName + lastName + email
+    res.redirect(returnURL)
+  } else {
+    res.redirect('?success=false')
+  }
 })
 
 var port = process.env.PORT;
